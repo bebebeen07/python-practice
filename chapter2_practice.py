7fae82f76a5219982557bb4cebc48ff3b8e64b39
bebebeen07 <tiffa-cheng@outlook.com>
2025-09-29 21:59:35 +0800

    add new practice in my repository

---
+++ chapter2_practice.py
@@ -0,0 +1,38 @@
+#2.1
+message="I am a smart person"
+print(message)
+#2.2
+message="12345"
+print(message)
+message="hello"
+print(message)
+#2.3
+name="eric"
+print(f"Hello {name.title()},would you like to learn some python today?")
+#2.4
+name="lisa april"
+print(name.upper())
+print(name.lower())
+print(name.title())
+#2.5
+name="albert einstein"
+words="A person who never made a mistake never tried anything new."
+print(f'{name.title()} once said,"{words}"')
+#2.6
+famous_person="albert einstein"
+message=famous_person
+words="A person who never made a mistake never tried anything new."
+print(f'{name.title()} once said,"{words}"')
+#2.7
+name="\ntiffa cheng"
+print(name.title())
+name="\ttiffa cheng"
+print(name.title())
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+#2.8
+file_name="python_notes.txt"
+file_name=file_name.removesuffix(".txt")
+print(file_name)
+import this
